231e925a5b680c144c299a5d7b24b7c98ee7924c
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-29 11:09:27 +0530

    dlt

---
+++ src/app/search-delete/search-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-delete',
+  templateUrl: './search-delete.component.html',
+  styleUrls: ['./search-delete.component.css']
+})
+export class SearchDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
