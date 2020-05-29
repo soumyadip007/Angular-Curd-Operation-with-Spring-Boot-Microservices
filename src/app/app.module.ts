dfc87a9b7e0f2238500c58060498985d3adee97a
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-29 11:08:02 +0530

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SearchDeleteComponent } from './search-delete/search-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    SearchDeleteComponent
   ],
   imports: [
     BrowserModule
