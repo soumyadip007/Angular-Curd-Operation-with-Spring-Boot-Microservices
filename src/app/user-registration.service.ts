02234897c9dcebbcaf26150f2df6e5941f124fd4
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-29 11:49:58 +0530

    user reg

--- src/app/user-registration.service.ts
+++ src/app/user-registration.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRegistrationService {
 
-  constructor() { }
+  constructor(http:HttpClient) { }
+
+  public doReg(user){
+    return this.http.post();
+  }
 }
