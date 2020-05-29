4544bbe802d179ff0c464e6e8146d9aafcc92bce
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-29 13:48:11 +0530

    Search component

--- src/app/search-delete/search-delete.component.ts
+++ src/app/search-delete/search-delete.component.ts
@@ -9,10 +9,27 @@ import { UserRegistrationService } from '../user-registration.service';
 export class SearchDeleteComponent implements OnInit {
 
   users:any;
+  email:string;
+  find:string;
   constructor(private service:UserRegistrationService) { }
 
   ngOnInit() {
     let res=this.service.doGetAll();
+    res.subscribe((data)=>this.users=data);
+  }
+
+  
+  public delteUser(id:string){
+    let res=this.service.doDelete(id);
+    res.subscribe((data)=>this.users=data);
+  }
+
+
+  
+  public findById(find:string){
+    console.log(find);
+    console.log('Ji');
+    let res=this.service.doGetId('12');
     res.subscribe((data)=>this.users=data);
   }
 
