9010ed99830eb47b6af3bcdbdef597f7214ac806
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-29 12:22:01 +0530

    reg

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { UserRegistrationService } from '../user-registration.service';
 
 @Component({
   selector: 'app-registration',
@@ -9,10 +10,16 @@ import { User } from '../user';
 export class RegistrationComponent implements OnInit {
 
   user:User=new User("","",0);
+  message:any;
 
-  constructor() { }
+  constructor(private service: UserRegistrationService) { }
 
   ngOnInit() {
+  }
+
+  public registerNow(){
+    let res=this.service.doReg(this.user);
+    res.subscribe((data)=>this.message=data);
   }
 
 }
